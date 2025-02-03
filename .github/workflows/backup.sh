7d44882c8e80963e67bc66fd048f0838ee540fa3
hexpunk <10983817+hexpunk@users.noreply.github.com>
2025-02-03 11:50:58 -0600

    Update backup.sh
    Add missing backslash

--- .github/workflows/backup.sh
+++ .github/workflows/backup.sh
@@ -16,7 +16,7 @@ wget --wait=2 \
      --span-hosts \
      --domains="jaysherby.com,digitaloceanspaces.com" \
      --exclude-directories="hit,upvote" \
-     --regex-type pcre
+     --regex-type pcre \
      --reject-regex="[?&]q=" \
      -e robots=off \
      https://jaysherby.com
